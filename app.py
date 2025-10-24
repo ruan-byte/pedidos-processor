810499b5b7dafcf5cba1bec0b330f24a147036d0
ruan-byte <ruan@asitechgroup.com.br>
2025-10-24 14:41:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -40,13 +40,13 @@ async def processar_pedidos(request: Request):
                 continue
             
             try:
-                pedido = {
-                    "data": cells[0].get_text(strip=True),
-                    "nr_pedido": cells[2].get_text(strip=True),
-                    "cliente": cells[4].get_text(strip=True),
-                    "vendedor": cells[6].get_text(strip=True),
-                    "total": cells[10].get_text(strip=True).replace('.', '').replace(',', '.')
-                }
+          pedido = {
+        "data_pedido": cells[0].get_text(strip=True),  # ← Mude "data" para "data_pedido"
+        "nr_pedido": cells[2].get_text(strip=True),
+        "cliente": cells[4].get_text(strip=True),
+        "vendedor": cells[6].get_text(strip=True),
+        "total": cells[10].get_text(strip=True).replace('.', '').replace(',', '.')
+    }
                 pedidos.append(pedido)
             except:
                 continue
